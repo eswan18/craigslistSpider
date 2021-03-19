830e46b9942f28182758a0ba833421504e49bf5a
Ethan Swan <eswan18@rocketmail.com>
2021-03-19 07:25:23 -0500

    Limit scraping speed.

--- craigslistSpider/settings.py
+++ craigslistSpider/settings.py
@@ -16,20 +16,20 @@ DOWNLOADER_CLIENT_TLS_METHOD = "TLSv1.2"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'craigslistSpider (+http://www.yourdomain.com)'
+USER_AGENT = 'craigslistSpider'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+#CONCURRENT_REQUESTS = 8
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 8
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
